e19fdd106b301f8dbe39cbd33e7a85a11eab2aa3
Mickael Ferreira <ferreirarmickael@gmail.com>
2023-01-16 14:14:53 +0100

    debug fonction

--- Js/fonctionsPage.js
+++ Js/fonctionsPage.js
@@ -81,3 +81,38 @@ function pageGestionProfile() {
     },
   });
 }
+function pageDemande() {
+  $.ajax({
+    url: "../controller/ControllerRoute.php",
+    dataType: "json",
+    type: "POST",
+    data: {
+      request: "pageDemande",
+    },
+    success: function (response) {
+      $("#page").html(response);
+    },
+    error: function () {
+      alert("Error !");
+    },
+  });
+}
+
+function pageGestionMateriel() {
+  loadMateriel(1);
+  $.ajax({
+    url: "../controller/ControllerRoute.php",
+    dataType: "json",
+    type: "POST",
+    data: {
+      request: "pageGestionMateriel",
+    },
+    success: function (response) {
+      loadMateriel(1);
+      $("#page").html(response);
+    },
+    error: function () {
+      alert("Error !");
+    },
+  });
+}
