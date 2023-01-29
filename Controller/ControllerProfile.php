6de3cf0a40aaf4727c424314b41c0aaf5813a7c2
alexis20112003 <104432390+alexis20112003@users.noreply.github.com>
2023-01-29 15:24:16 +0100

    debug update image profile

--- Controller/ControllerProfile.php
+++ Controller/ControllerProfile.php
@@ -1,32 +1,36 @@
 <?php
-// require_once('../vendor/autoload.php');
-// require_once('../Entity/Database.php');
-// require_once('../Entity/User.php');
-// $db = new Database();
-// $GLOBALS['database'] = $db->mysqlConnexion();
+require_once('../vendor/autoload.php');
+require_once('../Entity/Database.php');
+require_once('../Entity/User.php');
+$db = new Database();
+$GLOBALS['database'] = $db->mysqlConnexion();
 
-// session_start();
-
-
-// $render = new \Twig\Loader\FilesystemLoader('../components/');
-
-// $twig = new \Twig\Environment($render);
-
-// $reussite = 0;
-// $statut = 0;
-// $msg = "";
+session_start();
 
 
-// if (isset($_FILES)){
-//     $list_image = [
-//         "img_url" => 'gestion_materiel/Assets/' .$_FILES['new_file']['name'],
-//         'img_file' => $_FILES['new_file']['tmp_name']
-//     ];
+$render = new \Twig\Loader\FilesystemLoader('../components/');
 
-//     $list = [
-//         'title'=> htmlspecialchars($_FILES['new_file']['name']),
-//         'img_url' => $list_image['img_url']
-//     ];
+$twig = new \Twig\Environment($render);
 
-//     move_uploaded_file($list_image['img_file'], $list_image['img_url']);
-// }
+$reussite = 0;
+$statut = 0;
+$msg = "";
+
+
+if (isset($_FILES)) {
+    error_log(print_r($_FILES, true));
+    $msg = 'c bon';
+    $response = $msg;
+    // $list_image = [
+    //     "img_url" => 'gestion_materiel/Assets/' . $_FILES['new_file']['name'],
+    //     'img_file' => $_FILES['new_file']['tmp_name']
+    // ];
+
+    // $list = [
+    //     'title' => htmlspecialchars($_FILES['new_file']['name']),
+    //     'img_url' => $list_image['img_url']
+    // ];
+
+    // move_uploaded_file($list_image['img_file'], $list_image['img_url']);
+    echo json_encode($response);
+}
