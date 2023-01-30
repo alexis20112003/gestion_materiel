2ec8b6d84a9bb371b80d426c868fd9a8ca26fe65
alexis20112003 <104432390+alexis20112003@users.noreply.github.com>
2023-01-30 09:50:56 +0100

    Update Main.php

--- templates/Main.php
+++ templates/Main.php
@@ -16,12 +16,10 @@ $preparedSql = $GLOBALS['database']->prepare('SELECT * FROM `site` ');
 $preparedSql->execute();
 $sites = $preparedSql->fetchAll(PDO::FETCH_ASSOC);
 
-error_log(json_encode($_SESSION));
 if (isset($_SESSION['id'])) {
-  
-  $id= User::encrypt_decrypt('decrypt', $_SESSION['id']);
+
+  $id = User::encrypt_decrypt('decrypt', $_SESSION['id']);
   $user = new User($id);
-  error_log(json_encode($id));
   echo $twig->render('header.html.twig') . $twig->render('main.html.twig', array(
     "user" => $user,
     "sites" => $sites,
