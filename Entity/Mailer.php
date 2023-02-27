e3cb42f5d1bf21343a216fcadc3fc05dea3aa77d
alexis20112003 <104432390+alexis20112003@users.noreply.github.com>
2023-02-27 15:12:16 +0100

    update

--- Entity/Mailer.php
+++ Entity/Mailer.php
@@ -78,4 +78,72 @@ class Mailer
         }
         return $message;
     }
+
+    public function sendMailAccept($userEmail, $info)
+    {
+        $render = new \Twig\Loader\FilesystemLoader('../components/');
+        $twig = new \Twig\Environment($render);
+
+        $to = $userEmail;
+
+        $mail = new PHPMailer(true);
+        $mail->isSMTP();
+
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = "tls";
+        $mail->Host = "smtp-mail.outlook.com";  //outlook SMTP server
+        $mail->Port = 587;                    //SMTP port
+        try {
+            $mail->Username = "ProjetG-Materiel@outlook.fr";
+            $mail->Password = "0Gestion_Materiel0";
+
+            $mail->setFrom("ProjetG-Materiel@outlook.fr", "Administrateur");
+            $mail->addAddress($to, "admin");
+
+            $mail->IsHTML(true);
+            $mail->Subject = 'Bienvenue';
+            $mail->MsgHTML($twig->render('emailBodyAccept.html.twig', array(
+                "infos" => $info,
+            )));
+            $mail->send();
+            $message = 'Le mail a bien été envoyé.';
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return $message;
+    }
+
+    public function sendMailRefuse($userEmail, $info)
+    {
+        $render = new \Twig\Loader\FilesystemLoader('../components/');
+        $twig = new \Twig\Environment($render);
+
+        $to = $userEmail;
+
+        $mail = new PHPMailer(true);
+        $mail->isSMTP();
+
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = "tls";
+        $mail->Host = "smtp-mail.outlook.com";  //outlook SMTP server
+        $mail->Port = 587;                    //SMTP port
+        try {
+            $mail->Username = "ProjetG-Materiel@outlook.fr";
+            $mail->Password = "0Gestion_Materiel0";
+
+            $mail->setFrom("ProjetG-Materiel@outlook.fr", "Administrateur");
+            $mail->addAddress($to, "admin");
+
+            $mail->IsHTML(true);
+            $mail->Subject = 'Bienvenue';
+            $mail->MsgHTML($twig->render('emailBodyRefuse.html.twig', array(
+                "infos" => $info,
+            )));
+            $mail->send();
+            $message = 'Le mail a bien été envoyé.';
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return $message;
+    }
 }
